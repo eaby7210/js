174721681a7354cc0e0bf2099208d6fa318fd8ba
Eaby Thomas C <eabythomascu@gmail.com>
2023-08-02 21:26:49 +0530

    js updated no error css updated for ref

--- movie finder/mFinder.js
+++ movie finder/mFinder.js
@@ -4,7 +4,8 @@ const API_URL_SEARCH="http://www.omdbapi.com/?apikey=df32bf7d&i=";
 
 document.addEventListener("DOMContentLoaded", function () {
     let searchInput = document.getElementById("form-input");
-    let card = document.getElementsByClassName("movie-card")[0];
+    let card = document.getElementsByClassName("movie-cards")[0];
+
 
 
 
@@ -16,18 +17,30 @@ document.getElementsByClassName("search")[0].addEventListener("click",function()
         console.log(query);
         getMovies(API_URL+query);
     }
-})
+});
 console.log(API_URL+newFunction());
 function newFunction() {
     return "avatar";
 }
 
 async function getMovies(url){
-    const resp = await fetch(url);
+    /*const resp = await fetch(url);
     const respData = await resp.json();
     console.log(respData);
     showMovies(respData.search);
-    console.log(respData.search);
+    console.log(respData.search);*/
+    try {
+        const resp = await fetch(url);
+        const respData = await resp.json();
+        if (respData.Response === 'True') {
+          showMovies(respData.Search);
+          console.log(respData);
+        } else {
+          console.error(respData.Error);
+        }
+      } catch (error) {
+        console.error('Error fetching data from the API:', error);
+      }
 }
 
 function showMovies(movies){
@@ -43,12 +56,12 @@ function showMovies(movies){
 
 function movie_display(imovie){
     const movieElm = document.createElement("div");
-    movieElm.classList.add ("movie-card");
+    movieElm.classList.add ("movie-cards");
     movieElm.innerHTML=`
     <div class="card">
         <img src="${imovie.Poster}" alt="poster" height="300px" width="300px"/>
     <br>
-    <div class="movie-desction">
+    <div class="movie-desc">
         <span class=movie-title><b>Title</b><span class="value">${imovie.Title} </span>
         <span class=movie-title><b>Rating</b><span class="value">${imovie.imdbRating} </span>
         <span class=movie-title><b>Director</b><span class="value">${imovie.Director} </span>
@@ -61,4 +74,4 @@ function movie_display(imovie){
    card.appendChild(movieElm);
 }
 
-
+});
